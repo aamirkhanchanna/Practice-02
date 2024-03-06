79e2b937f839c388bd5f4c6c2f04c3dfd69f38a5
Aamir Khan Channa <160345903+aamirkhanchanna@users.noreply.github.com>
2024-03-06 22:51:07 +0500

    Add files via upload

---
+++ Self.ts
@@ -0,0 +1,16 @@
+let myName = "My Name Is Aamir Khan Channa";
+console.log(myName)
+let myFather = "My Father Name Is Ahmed Khan Channa";
+console.log(myFather)
+let favourateColor = "My Favourite Color Is Black";
+console.log(favourateColor)
+let isAge = "Iam Twenty Five Years Old"
+console.log(isAge)
+let howOld:number = 25
+console.log(howOld)
+let myVillage:String = "My Village Is So Beautifull";
+console.log(myVillage)
+let areYoustudent = "I Am Student In Governor Initiative IT Program AI WEB 3.O Metaverse";
+console.log(areYoustudent)
+let istudent:boolean = true
+console.log(istudent)
---
+++ self.js
@@ -0,0 +1,16 @@
+var myName = "My Name Is Aamir Khan Channa";
+console.log(myName);
+var myFather = "My Father Name Is Ahmed Khan Channa";
+console.log(myFather);
+var favourateColor = "My Favourite Color Is Black";
+console.log(favourateColor);
+var isAge = "Iam Twenty Five Years Old";
+console.log(isAge);
+var howOld = 25;
+console.log(howOld);
+var myVillage = "My Village Is So Beautifull";
+console.log(myVillage);
+var areYoustudent = "I Am Student In Governor Initiative IT Program AI WEB 3.O Metaverse";
+console.log(areYoustudent);
+var istudent = true;
+console.log(istudent);
